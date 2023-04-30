59a4c0871048b40d41bc4e2713274a6391e41c58
PietroBen <pietrobenedicenti@gmail.com>
2023-04-30 16:22:16 +0200

    Ho sistemato i primi

--- src/PrimiPiatti.java
+++ src/PrimiPiatti.java
@@ -9,38 +9,42 @@ public class PrimiPiatti {
       }
 
       public String getDescription() {
+
             return description;
       }
 
       public void setDescription(String description) {
+
             this.description = description;
       }
 
       public double getPrice() {
+
             return price;
       }
 
       public void setPrice(double price) {
+
             this.price = price;
       }
       public static void printPrimiPiatti() {
             PrimiPiatti primoPiatto = new PrimiPiatti("Pasta in bianco",5.00);
-            System.out.println(primoPiatto.getDescription() + "                      €" + primoPiatto.getPrice());
+            System.out.println(primoPiatto.getDescription() + "                      € " + primoPiatto.getPrice());
 
             primoPiatto.setDescription("Trofie al pesto");
             primoPiatto.setPrice(8.00);
-            System.out.println(primoPiatto.getDescription() + "                      €" + primoPiatto.getPrice());
+            System.out.println(primoPiatto.getDescription() + "                      € " + primoPiatto.getPrice());
 
             primoPiatto.setDescription("Lasagne alla bolognese");
             primoPiatto.setPrice(12.00);
-            System.out.println(primoPiatto.getDescription() + "               €" + primoPiatto.getPrice());
+            System.out.println(primoPiatto.getDescription() + "               € " + primoPiatto.getPrice());
 
             primoPiatto.setDescription("Gnocchi alla romana");
             primoPiatto.setPrice(9.00);
-            System.out.println(primoPiatto.getDescription() + "                  €" + primoPiatto.getPrice());
+            System.out.println(primoPiatto.getDescription() + "                  € " + primoPiatto.getPrice());
 
             primoPiatto.setDescription("Pappardelle al ragù di cinghiale");
             primoPiatto.setPrice(13.00);
-            System.out.println(primoPiatto.getDescription() + "     €" + primoPiatto.getPrice());
+            System.out.println(primoPiatto.getDescription() + "     € " + primoPiatto.getPrice());git
       }
 }
