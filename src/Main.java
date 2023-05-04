acbe73a1c35bf63535b58172ed6fb25add30e49f
Rocco <rocco.tripodi1994@gmail.com>
2023-05-04 19:21:26 +0200

    Aggiunto nome ristorante

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         //stampare il menù
+        String nameOfRestaurant = "Ristorante ManJava";
+        System.out.println(nameOfRestaurant);
 
         Bevande.stampaBevande();
 
