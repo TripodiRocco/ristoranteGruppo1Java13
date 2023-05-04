845bc1db89c09c3036535ddb01d84f085bce63ee
Giuseppe <giuseppebron99@gmail.com>
2023-05-04 19:25:20 +0200

    add Class Dessert

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,12 @@ public class Main {
         Bevande.stampaBevande();
         Dessert tiramisu = new Dessert("Tiramisu'", 4.00);
         Dessert parfait = new Dessert("Parfait di mandorle", 5.00);
+        Dessert tortino = new Dessert("Tortino morbido al cioccolato fondente", 3.20);
+        Dessert sorbetto = new Dessert("Sorbetto al limone", 2.00);
 
         tiramisu.printDessert(tiramisu);
         tiramisu.printDessert(parfait);
+        tiramisu.printDessert(tortino);
+        tiramisu.printDessert(sorbetto);
     }
 }
