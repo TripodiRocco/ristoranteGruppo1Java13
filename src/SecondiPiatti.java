19027e6d178e84ce289c7843bb416c0167deaa60
vincenzo merola <vincenzo.merola18@gmail.com>
2023-05-04 17:11:57 +0200

    Aggiunta classe secondiPiatti

---
+++ src/SecondiPiatti.java
@@ -0,0 +1,29 @@
+public class SecondiPiatti {
+    private String description;
+    private double price;
+
+    public SecondiPiatti(String description, double price){
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void print(){
+        System.out.println(description + " price: " + price);
+    }
+}
