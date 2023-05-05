92b5c88e09174f5db95666944dfe82bfb5570ae4
Rocco <rocco.tripodi1994@gmail.com>
2023-05-05 17:23:04 +0200

    Metodo in classe Bevande modificato

--- src/Bevande.java
+++ src/Bevande.java
@@ -23,36 +23,7 @@ public class Bevande {
         this.price = price;
     }
 
-     public static void stampaBevande(){
-        Bevande bevande = new Bevande("Acqua minerale", 5.00);
-        System.out.println(bevande.getDescription() + "                 " + "€ " + bevande.getPrice());
-
-        bevande.setDescription("Amari");
-        bevande.setPrice(8.00);
-        System.out.println(bevande.getDescription() + "                          " + "€ " + bevande.getPrice());
-
-        bevande.setDescription("Liquori");
-        bevande.setPrice(7.00);
-        System.out.println(bevande.getDescription() + "                        " + "€ " + bevande.getPrice());
-
-        bevande.setDescription("Caffè");
-        bevande.setPrice(4.00);
-        System.out.println(bevande.getDescription() + "                          " + "€ " + bevande.getPrice());
-
-        bevande.setDescription("Thè ed infusi");
-        bevande.setPrice(6.00);
-        System.out.println(bevande.getDescription() + "                  " + "€ " + bevande.getPrice());
-
-        bevande.setDescription("Coca Cola");
-        bevande.setPrice(5.00);
-        System.out.println(bevande.getDescription() + "                      " + "€ " + bevande.getPrice());
-
-        bevande.setDescription("Fanta");
-        bevande.setPrice(5.00);
-        System.out.println(bevande.getDescription() + "                          " + "€ " + bevande.getPrice());
-
-        bevande.setDescription("Sprite");
-        bevande.setPrice(5.00);
-        System.out.println(bevande.getDescription() + "                         " + "€ " + bevande.getPrice());
+     public void stampaBevande(){
+         System.out.println(this.description + "   € " + this.price);
     }
 }
