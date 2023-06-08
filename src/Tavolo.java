5474adbb9174e382d412d1e6b549c583db53ecff
Giuseppe Bronzellino <129170494+Giuseppebron@users.noreply.github.com>
2023-06-08 15:28:33 +0200

    add classe tavolo

---
+++ src/Tavolo.java
@@ -0,0 +1,25 @@
+public class Tavolo {
+    private TipoTavolo numeroPosti;
+    private int numeroTavolo;
+
+    public Tavolo(TipoTavolo numeroPosti, int numeroTavolo) {
+        this.numeroPosti = numeroPosti;
+        this.numeroTavolo = numeroTavolo;
+    }
+
+    public int getNumeroTavolo() {
+        return numeroTavolo;
+    }
+
+    public void setNumeroTavolo(int numeroTavolo) {
+        this.numeroTavolo = numeroTavolo;
+    }
+
+    public TipoTavolo getNumeroPosti() {
+        return numeroPosti;
+    }
+
+    public void setNumeroPosti(TipoTavolo numeroPosti) {
+        this.numeroPosti = numeroPosti;
+    }
+}
